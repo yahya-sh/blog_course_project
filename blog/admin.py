11bcbaf07d066598f5cee74a15fcc0ff617d055b
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-09 13:19:54 +0300

    customized post admin class

--- blog/admin.py
+++ blog/admin.py
@@ -4,9 +4,13 @@ from . import models
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "tag_list", "date")
+    list_filter = ("author", "tags", "date")
     prepopulated_fields = {
         "slug": ("title",),
     }
+    def tag_list(self, obj):
+        return ", ".join(tag.caption for tag in obj.tags.all())
 
 
 admin.site.register(models.Author)
