bfd209f208e59fdc0ba0c76c4795d06b6dd83d59
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-06-09 13:12:50 +0300

    repopulate slug column based on title column in admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from . import models
 
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {
+        "slug": ("title",),
+    }
+
+
 admin.site.register(models.Author)
-admin.site.register(models.Post)
+admin.site.register(models.Post, PostAdmin)
 admin.site.register(models.Tag)
